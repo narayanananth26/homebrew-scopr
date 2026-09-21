00bc68ddf070141582997b9d149f2ed902cad393
Ananth Narayan <ananth.narayan@goodlife.company>
2026-09-21 11:02:51 +0530

    scopr 0.1.0

---
+++ Formula/scopr.rb
@@ -0,0 +1,19 @@
+class Scopr < Formula
+  desc "Launch Claude Code scoped to chosen repositories"
+  homepage "https://github.com/narayanananth26/scopr"
+  url "https://github.com/narayanananth26/scopr/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "REPLACE_AFTER_TAGGING"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/scopr"
+    zsh_completion.install "completions/_scopr"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/scopr version")
+    assert_match "#compdef scopr", shell_output("#{bin}/scopr completion zsh")
+  end
+end
